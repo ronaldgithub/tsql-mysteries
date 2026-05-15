3b0b685a05961ecb487f7fa7d4af28345b5bb679
Ronald de Groot <Ronald.de.Groot@live.nl>
2026-05-15 11:22:23 +0200

    Added a version comment.

--- check-version.sql
+++ check-version.sql
@@ -1,1 +1,7 @@
 SELECT @@VERSION AS version;
+/*
+version
+Microsoft SQL Server 2025 (RTM-GDR) (KB5091223) - 17.0.1115.1 (X64) Apr 19 2026 01:00:58   
+Copyright (C) 2025 Microsoft Corporation  Enterprise Developer Edition (64-bit) 
+  on Windows 10 Pro 10.0 <X64>  (Build 19045: ) (Hypervisor) 
+*/
